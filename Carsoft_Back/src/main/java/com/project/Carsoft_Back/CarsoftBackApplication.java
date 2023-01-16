1ae9fe163b2963c1f9a27f790b869595ef867277
slixz0123 <slim.ph2002@gmail.com>
2023-01-15 21:45:11 -0500

    update

---
+++ Carsoft_Back/src/main/java/com/project/Carsoft_Back/CarsoftBackApplication.java
@@ -0,0 +1,13 @@
+package com.project.Carsoft_Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsoftBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsoftBackApplication.class, args);
+	}
+
+}
---
+++ Carsoft_Back/src/test/java/com/project/Carsoft_Back/CarsoftBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Carsoft_Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsoftBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
